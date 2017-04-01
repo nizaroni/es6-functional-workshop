7e501f420783c3dfc857f52e0027a43bc108c5f3
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-04-01 15:56:12 -0400

    es6: Add solutions for exercise 4

--- es6/exercises/ex4/ex4.js
+++ es6/exercises/ex4/ex4.js
@@ -1,10 +1,22 @@
-function upper(strings,...values) {}
+function upper(strings,...values) {
+  var str = ''
+
+  for (let i = 0; i < strings.length; i += 1) {
+    if (i > 0) {
+      str += values[i-1].toString().toUpperCase()
+    }
+    str += strings[i]
+  }
+
+  return str
+}
 
 var name = "kyle",
 	twitter = "getify",
 	classname = "es6 workshop";
 
 console.log(
-	`Hello ____ (@____), welcome to the ____!` ===
+  upper
+	`Hello ${name} (@${twitter}), welcome to the ${classname}!` ===
 	"Hello KYLE (@GETIFY), welcome to the ES6 WORKSHOP!"
 );

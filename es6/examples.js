c3b200d3a3bc5b33a11ff07dae164aec465a2831
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-04-01 15:42:53 -0400

    es6: Add backtick string examples

--- es6/examples.js
+++ es6/examples.js
@@ -46,3 +46,26 @@ var {
 = pizza
 
 console.log({ baconA, baconB })
+
+
+// tagged interpolitral
+var amount = 12.3
+var msg = formatCurrency `The total for your order is ${amount}.`
+console.log(msg)
+
+function formatCurrency (strings, ...values) {
+  var str = ''
+
+  for (let i = 0; i < strings.length; i++) {
+    if (i > 0) {
+      if (typeof values[i-1] == 'number') {
+        str += `$${values[i-1].toFixed(2)}`
+      }
+      else {
+        str += values[i-1]
+      }
+    }
+    str += strings[i]
+  }
+  return str
+}

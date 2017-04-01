1cb2af5895cc9c93dcb7e3747b0cbbb813fd8b69
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-04-01 12:28:08 -0400

    es6: Add solutions for exercise 2

--- es6/exercises/ex2/ex2-fixed.js
+++ es6/exercises/ex2/ex2-fixed.js
@@ -1,12 +1,12 @@
-function foo(head, dontCareA, dontCareB, ...tail) {
-  return [head, ...tail];
+function foo(x,y,z,...rest) {
+	return [ x, ...rest ];
 }
 
 function bar() {
 	var a1 = [ 2, 4 ];
 	var a2 = [ 6, 8, 10, 12 ];
 
-	return foo(...a1, ...a2);
+	return foo( ...a1, ...a2 );
 }
 
 console.log(

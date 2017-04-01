b793d2333c1d3e97f7983425a943bc70e8ed4dc0
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-04-01 16:38:09 -0400

    es6: Add solutions for exercise 5

--- es6/exercises/ex5/ex5.js
+++ es6/exercises/ex5/ex5.js
@@ -1,13 +1,31 @@
 var numbers = {
-	// ..
+  *[Symbol.iterator]() {
+    for (let i = this.start; i <= this.end; i += this.step) {
+      yield i
+    }
+  },
+
+  configure(
+    {
+      start = 0,
+      end = 100,
+      step = 1
+    } = {}
+  ) {
+    this.start = start
+    this.end = end
+    this.step = step
+  }
 };
 
+numbers.configure()
 // should print 0..100 by 1s
 for (let num of numbers) {
 	console.log(num);
 }
 
+numbers.configure({ start: 6, end: 30, step: 4 })
 // should print 6..30 by 4s
-for (let num of /*..*/) {
+for (let num of numbers) {
 	console.log(num);
 }

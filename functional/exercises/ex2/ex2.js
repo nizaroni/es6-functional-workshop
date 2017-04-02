45b6c58411c96617ee4042bb8c5373494329606c
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-04-02 11:30:24 -0400

    functional: Add solutions for exercise 2

--- functional/exercises/ex2/ex2.js
+++ functional/exercises/ex2/ex2.js
@@ -3,14 +3,31 @@ function decrement(x) { return x - 1; }
 function double(x) { return x * 2; }
 function half(x) { return x / 2; }
 
-function compose() {}
-function pipe() {}
+function compose(...funcs) {
+  funcs.reverse()
+
+  return function composition(arg) {
+    for (func of funcs) {
+      arg = func(arg)
+    }
+    return arg
+  }
+}
+
+function pipe(...funcs) {
+  return function pipeline(arg) {
+    for (func of funcs) {
+      arg = func(arg)
+    }
+    return arg
+  }
+}
 
 var f = compose(decrement,double,increment,half);
 var p = pipe(half,increment,double,decrement);
 
-f(3) === 4;
+console.log( f(3) === 4 );
 // true
 
-f(3) === p(3);
+console.log( f(3) === p(3) );
 // true

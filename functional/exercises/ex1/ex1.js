a5123b24dedcb24f631e03e998bd6d60f87d9d9e
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-04-02 12:51:37 -0400

    functional: Add solutions for exercise 1

--- functional/exercises/ex1/ex1.js
+++ functional/exercises/ex1/ex1.js
@@ -1,3 +1,7 @@
+console.log('')
+console.log('impure foo')
+console.log('----------')
+
 function foo(x) {
 	y++;
 	z = x * y;
@@ -7,6 +11,76 @@ var y = 5, z;
 
 foo(20);
 z;		// 120
+console.log({ y, z })
 
 foo(25);
 z;		// 175
+console.log({ y, z })
+console.log('')
+
+
+//---------------------------------------
+
+
+console.log('pure foo')
+console.log('--------')
+
+function pureFoo(x, y) {
+  y += 1
+  return [y, x * y]
+}
+
+;[y, z] = pureFoo(20, 5)
+console.log({ y, z })
+
+;[y, z] = pureFoo(25, 6)
+console.log({ y, z })
+console.log('')
+
+
+//---------------------------------------
+
+
+console.log('wrapped foo')
+console.log('-----------')
+
+function wrappedFoo(x, y) {
+  var z
+  foo(x)
+  return [y, z]
+
+  function foo(x) {
+    y++
+    z = x * y
+  }
+}
+
+;[y, z] = wrappedFoo(20, 5)
+console.log({ y, z })
+
+;[y, z] = wrappedFoo(25, 6)
+console.log({ y, z })
+console.log('')
+
+
+//---------------------------------------
+
+
+console.log('reset foo')
+console.log('---------')
+
+function resetFoo(xArg, yArg) {
+  var [oldY, oldZ] = [y, z]
+  y = yArg
+  foo(xArg)
+  var [newY, newZ] = [y, z]
+  ;[y, z] = [oldY, oldZ]
+  return [newY, newZ]
+}
+
+;[y, z] = resetFoo(20, 5)
+console.log({ y, z })
+
+;[y, z] = resetFoo(25, 6)
+console.log({ y, z })
+console.log('')

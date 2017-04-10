d2208cf645a2fe8d90dd00d33c75e057d2914925
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-04-10 00:56:43 -0400

    functional: Add solutions for archived exercise

--- functional/exercises/ex3-archived/ex3.js
+++ functional/exercises/ex3-archived/ex3.js
@@ -1,7 +1,10 @@
-function mult(x,y,z) {
-	return x * y * z;
+function mult (x, y, ...remainingNumbers) {
+  if (remainingNumbers.length === 0) {
+    return x * y
+  }
+  return mult(x * y, ...remainingNumbers)
 }
 
-mult(3,4,5);	// 60
+console.log( mult(3,4,5) )   // 60
 
-mult(3,4,5,6);	// Oops!
+console.log( mult(3,4,5,6) ) // 360
